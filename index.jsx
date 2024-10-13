a38150f7e763ce43226a1a15e13da717b984ea6b
Sinaye <sinaye.mbele@gmail.com>
2024-10-13 20:28:31 +0200

    Challenge attempt

---
+++ index.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthRequired from "./components/AuthRequired";
+import HostLayout from "./components/HostLayout";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Host/Dashboard";
+import HostVanDetail from "./pages/Host/HostVanDetail";
+import HostVanInfo from "./pages/Host/HostVanInfo";
+import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import HostVanPricing from "./pages/Host/HostVanPricing";
+import HostVans from "./pages/Host/HostVans";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import VanDetail from "./pages/Vans/VanDetail";
+import Vans from "./pages/Vans/Vans";
+
+import "./server";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
